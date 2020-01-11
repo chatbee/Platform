975b60c69643c5aafe76314b55b281f753cdc1bd
atotalnoob <atotalnoob@users.noreply.github.com>
2020-01-11 15:01:16 -0800

    Added headless testing for nodejs on ci

--- Platform/ClientApp/e2e/src/app.e2e-spec.ts
+++ Platform/ClientApp/e2e/src/app.e2e-spec.ts
@@ -8,16 +8,26 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display title', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('ClientApp app is running!');
+    expect(page.getTitleText()).toEqual('Chatbees Platform');
+  });
+
+  it('should display login title', () => {
+    page.navigateTo();
+    expect(page.getLoginTitleText()).toEqual('Chatbees Admin Login');
   });
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
+    const logs = await browser
+      .manage()
+      .logs()
+      .get(logging.Type.BROWSER);
+    expect(logs).not.toContain(
+      jasmine.objectContaining({
+        level: logging.Level.SEVERE
+      } as logging.Entry)
+    );
   });
 });
